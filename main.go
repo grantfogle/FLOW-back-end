5486f78f5b7c9356ed7401e94f6bc479116b26d7
Grant <grant.fogle@davita.com>
2020-06-03 16:24:33 -0600

    created a rivers struct and shows up in json format on allrivers endpoint

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type River struct {
+	Name        string `json:"Name"`
+	Desc        string `json:"desc"`
+	CurrentFlow int    `json:"currentFlow"`
+}
+
+type Rivers []River
+
+func allRivers(w http.ResponseWriter, r *http.Request) {
+	rivers := Rivers{
+		River{Name: "Colorado River", Desc: "The Colorado River is 2000 Miles in length", CurrentFlow: 800},
+	}
+	fmt.Println("Endpoint Hit: All Rivers Endpoint")
+	json.NewEncoder(w).Encode(rivers)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
@@ -12,6 +29,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/allrivers", allRivers)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
